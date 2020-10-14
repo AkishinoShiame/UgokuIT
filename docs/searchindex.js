b0b10bc7a8bf2a09c23bd97b6f8c27c8814ebeeb
akishinoshiame <akishinoshiame@icloud.com>
2020-10-14 13:55:24 +0800

    update html

---
+++ docs/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to \u52d5\u304f\uff01IT's documentation!"],titleterms:{"\u52d5\u304f":0,document:0,indic:0,tabl:0,welcom:0}})
