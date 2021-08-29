e8d8864f85bebbd4be1924a02db98dfa0ace55a4
AkishinoShiame <akishinoshiame@icloud.com>
2021-08-30 00:47:20 +0800

    update favicon

--- source/source/conf.py
+++ source/source/conf.py
@@ -108,12 +108,15 @@ html_static_path = ['_static']
 #
 # This is required for the alabaster theme
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
-html_sidebars = {
-    '**': [
-        'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
-    ]
-}
+# html_sidebars = {
+#     '**': [
+#         'relations.html',  # needs 'show_related': True theme option to display
+#         'searchbox.html',
+#     ]
+# }
+
+# Custom by Shiame, adding favicon to site
+html_favicon = 'favicon.ico'
 
 html_search_language = 'ja'
 
