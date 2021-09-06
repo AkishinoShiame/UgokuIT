6c891985bf507d166e5ab007d16c9da5240174e3
AkishinoShiame <akishinoshiame@icloud.com>
2021-09-06 20:53:28 +0800

    コード部分をコピペー機能追加。

--- source/source/conf.py
+++ source/source/conf.py
@@ -35,6 +35,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.imgmath',
     'sphinx.ext.githubpages',
+    'sphinx_copybutton',
     "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
